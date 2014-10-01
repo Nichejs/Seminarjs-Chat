b8c8c8d25cb20a149f71b1ab5d09d4c9d59c35a3
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-10-01 18:55:32 +0200

    Fixed some bugs

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-if (typeof (App) == 'undefined') {
-	console.error('ERROR: Seminarjs not detected');
-	process.exit(-1);
-}
-
-App.express.use('/plugins/chat/', function (req, res) {
-	if (req.method !== 'get') {
-		next();
-		return;
+module.exports = function (App) {
+	if (typeof (App) == 'undefined') {
+		console.error('ERROR: Seminarjs not detected');
+		process.exit(-1);
 	}
-	res.sendFile("./public/" + req.path);
-});
 
-var Chat = require('./src/chat-server.js');
-Chat(App.io);
+	try {
+
+		App.express.use('/plugins/chat/', function (req, res, next) {
+			if (req.method !== 'GET') {
+				next();
+				return;
+			}
+			res.sendFile(__dirname + "/public/" + req.path);
+		});
+
+		var Chat = require('./src/chat-server.js');
+		Chat(App.io);
+
+	} catch (e) {
+		console.error(e.getMessage());
+	}
+};
